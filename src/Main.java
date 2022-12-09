884e2725c72751c34b81a7b5d7a983b538312e75
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-09 12:28:51 +0100

    Commit init

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=15;
+        a=+5;
+        a=-4;
+        int b=a;
+        b++;
+
+        if((b%2==0)&&((b*(b=+1))%3==0)){
+            System.out.println("Both are true");
+        }else{
+            System.out.println("One or both aren't true");
+        }
+    }
+}
